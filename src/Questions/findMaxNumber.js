b12fa5d4b103a2711c5f9ae33eaa31195628d45a
Anas-Faiz <anasfaiz0811@gmail.com>
2025-11-11 00:48:33 -0800

    find max number

---
+++ src/Questions/findMaxNumber.js
@@ -0,0 +1,19 @@
+function findMaxNumber(arr) {
+    // Your implementation
+    let n = arr.length;
+
+    if (n == 0) return null;
+
+    let max = arr[0];
+
+    for (let i = 0; i < n; i++){
+        let value = arr[i];
+        if (value > max) max = value; 
+    }
+    return max;
+}
+
+//For the purpose of user debugging.
+findMaxNumber([1, 2, 3, 4, 5]);
+
+module.exports = findMaxNumber
