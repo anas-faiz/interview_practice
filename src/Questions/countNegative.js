f5424d598ae5053cd437d9e207dc6bdc0e6720e1
Anas-Faiz <anasfaiz0811@gmail.com>
2025-11-12 01:13:54 -0800

    countNegative

---
+++ src/Questions/countNegative.js
@@ -0,0 +1,15 @@
+function countNegatives(arr) {
+  // implement your solution here
+  if (!Array.isArray(arr)) return false
+  let count = 0
+  for (let i = 0; i < arr.length; i++){
+    let value = arr[i];
+    if (typeof value != 'number' || !Number.isFinite(value)) return false;
+    if (value < 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+module.exports = { countNegatives };
