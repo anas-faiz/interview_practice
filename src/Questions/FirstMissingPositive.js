4daa4f799760bb01fe38b57c46f7ce9342ad0647
Anas-Faiz <anasfaiz0811@gmail.com>
2025-11-15 00:54:18 -0800

    FirstMissingPositive

---
+++ src/Questions/FirstMissingPositive.js
@@ -0,0 +1,21 @@
+function firstMissingPositive(nums) {
+  // Your code here
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++){
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      const correctIndex = nums[i] - 1;
+      [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
+
+    }
+  }
+  for (let i = 0; i < n; i++){
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return n+1
+}
+
+module.exports = { firstMissingPositive };
