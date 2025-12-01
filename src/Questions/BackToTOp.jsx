fd448410c0b3afe47911df37f38f15909e959a24
Anas-Faiz <anasfaiz0811@gmail.com>
2025-11-30 22:59:57 -0800

    .

--- src/Questions/BackToTOp.jsx
+++ src/Questions/BackToTOp.jsx
@@ -1,23 +1,22 @@
 import { useEffect, useState } from "react";
 
 function BackToTop() {
-  const [isVisible, setIsVisible] = useState(false);  
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     try {
       function handleScroll() {
         if (window.scrollY > 300) {
-          setIsVisible(true)
-        }
-        else {
-          setIsVisible(false)
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
         }
       }
-      window.addEventListener("scroll", handleScroll)
+      window.addEventListener("scroll", handleScroll);
 
-      return () => window.removeEventListener("scroll", handleScroll)
+      return () => window.removeEventListener("scroll", handleScroll);
     } catch (error) {
-      console.log("Error: " + error.message)
+      console.log("Error: " + error.message);
     }
   }, []);
 
@@ -26,29 +25,28 @@ function BackToTop() {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
-    })
+    });
   }
   return (
     <div className="backToTop">
       <h1>Back To Top</h1>
 
       {/* Add some content to enable scrolling */}
-      {
-        [...Array(50)].map((_, i) => (
-          <p key={i}>this is paragraph {i+1}</p>
-        ))
-      }
-      
+      {[...Array(50)].map((_, i) => (
+        <p key={i}>this is paragraph {i + 1}</p>
+      ))}
+
       <div className="container">
         {/* Show this button only after scrolling down */}
         {isVisible && (
           <button
-          className="backtotop-btn"
-          onClick={scrollToTop}
-          data-testid="back-to-top-btn"
-        >
-          Back to Top
-        </button>)}
+            className="backtotop-btn"
+            onClick={scrollToTop}
+            data-testid="back-to-top-btn"
+          >
+            Back to Top
+          </button>
+        )}
       </div>
     </div>
   );
