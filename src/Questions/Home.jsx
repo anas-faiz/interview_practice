3ad6cdb986dfc97fa332e2f818145a7eedefd226
Anas-Faiz <anasfaiz0811@gmail.com>
2025-12-05 00:00:03 -0800

    Navbar Routing

---
+++ src/Questions/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';  
+
+const Home = () => {
+  return (
+    <div className="page-container">
+    <h1>Welcome to Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
