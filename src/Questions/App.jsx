b0e8493deac49c4f68eb2ca70c4fbede2697deb1
Anas-Faiz <anasfaiz0811@gmail.com>
2025-12-06 00:03:47 -0800

    .

--- src/Questions/App.jsx
+++ src/Questions/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import About from './About';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
@@ -10,8 +10,8 @@ const App = () => {
       <Navbar />
       <div className="page-content">
         <Routes>
-          <Route path = "/" element = {<Home/>} />
-          <Route path = "/about" element = {<About/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </Router>
